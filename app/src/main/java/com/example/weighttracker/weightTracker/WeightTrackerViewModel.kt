3c30bdba47a56f9be167cbea8198fd7c58b3d309
DukicDev <dukic.dev@gmail.com>
2021-02-17 20:52:29 +0100

    Update WeightTrackerViewModel.kt

--- app/src/main/java/com/example/weighttracker/weightTracker/WeightTrackerViewModel.kt
+++ app/src/main/java/com/example/weighttracker/weightTracker/WeightTrackerViewModel.kt
@@ -12,8 +12,6 @@ import kotlinx.coroutines.*
 class WeightTrackerViewModel(private val database: WeightDatabaseDao,
 application: Application): AndroidViewModel(application) {
 
-    //TODO: Fix adding after clear bug
-
     private var viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
     val allEntries = database.getAllEntries()
